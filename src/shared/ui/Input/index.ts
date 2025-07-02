d6f1cb5fa2d4c9568de230610bb2427e1214df83
CYBERWIZARD <mail.maxim6090@gmail.com>
2025-07-02 18:49:23 +0300

    fix voice input

--- src/shared/ui/Input/index.ts
+++ src/shared/ui/Input/index.ts
@@ -1,4 +1,4 @@
 export { InputPhone } from './input-phone/inputPhone';
 export { Input } from './input/input';
 export { Textarea } from './text-area/inputText';
-export { VoiceInput } from './input-voice/VoiceInput';
+export { VoiceInput } from './input-voice/inputVoice';
--- src/shared/ui/Input/input-voice/inputVoice.tsx
+++ src/shared/ui/Input/input-voice/inputVoice.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useSpeechRecognition } from 'shared/lib/hooks/useSpeechRecognition/useSpeechRecognition';
 import Mic from 'shared/assets/svg/bootstrap-icons-1.11.2/mic-fill.svg';
-import './VoiceInput.scss';
-export const VoiceInput: React.FC = () => {
+import './inputVoice.scss';
+export const VoiceInput = () => {
   const { transcript, isListening, startListening, error } = useSpeechRecognition();
 
   return (
