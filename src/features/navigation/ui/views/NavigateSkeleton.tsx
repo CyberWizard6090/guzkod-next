04a5eead421c2cbc86fe5bd20a7a479d9580e163
CYBERWIZARD <mail.maxim6090@gmail.com>
2025-08-09 22:26:35 +0300

    Add skeleton loader for navigation menu
    Introduced a NavigateSkeleton component with shimmer animation to display a loading state in the navigation menu. Updated NavigationView to use the new skeleton loader instead of the generic Loader. Added shimmer animation styles and adjusted related SCSS files for integration.

---
+++ src/features/navigation/ui/views/NavigateSkeleton.tsx
@@ -0,0 +1,20 @@
+import styles from './../styles/NavigateSkeleton.module.scss';
+
+export const NavigateSkeleton = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item} />
+      <div className={styles.item} />
+      <div className={styles.item} />
+      <div className={styles.item} />
+      <div className={styles.item} />
+      <div className={styles.item} />
+      <div className={styles.item} />
+      <div className={styles.item} />
+      <div className={styles.item} />
+      <div className={styles.item} />
+      <div className={styles.item} />
+      <div className={styles.item} />
+    </div>
+  );
+};
